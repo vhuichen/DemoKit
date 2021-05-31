1e5b9be4835cdb69fff46e20c1558aec9b657607
chenhui <chenhui@followme.cn>
2021-05-31 19:38:05 +0800

    31

---
+++ MachODemo/MachODemo/main.m
@@ -0,0 +1,25 @@
+//
+//  main.m
+//  MachODemo
+//
+//  Created by vchan on 2021/5/31.
+//
+
+//#import <UIKit/UIKit.h>
+//#import "AppDelegate.h"
+//
+//int main(int argc, char * argv[]) {
+//    NSString * appDelegateClassName;
+//    @autoreleasepool {
+//        // Setup code that might create autoreleased objects goes here.
+//        appDelegateClassName = NSStringFromClass([AppDelegate class]);
+//    }
+//    return UIApplicationMain(argc, argv, nil, appDelegateClassName);
+//}
+
+#import <stdio.h>
+
+int main(int argc, char * argv[]) {
+    printf("123");
+    return 0;
+}
