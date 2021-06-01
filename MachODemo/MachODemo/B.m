36cd9454be10b403be14b686be58209c322d64c4
chenhui <chenhui@followme.cn>
2021-06-01 20:27:07 +0800

    link

---
+++ MachODemo/MachODemo/B.m
@@ -0,0 +1,13 @@
+//
+//  B.m
+//  MachODemo
+//
+//  Created by vchan on 2021/6/1.
+//
+
+
+int global_var = 1;
+
+void func(int a) {
+    global_var = a;
+}
--- MachODemo/MachODemo/main.m
+++ MachODemo/MachODemo/main.m
@@ -17,9 +17,21 @@
 //    return UIApplicationMain(argc, argv, nil, appDelegateClassName);
 //}
 
-#import <stdio.h>
+//#import <stdio.h>
+//int main(int argc, char * argv[]) {
+//    static int iii = 0;
+//    static int jjj = 1;
+//
+//    printf("%d + %d",iii, jjj);
+//    return 0;
+//}
 
-int main(int argc, char * argv[]) {
-    printf("123");
+extern int global_var;
+
+void func(int a);
+
+int main() {
+    int a = 100;
+    func(a + global_var);
     return 0;
 }
