4b39a3debabcd2846aca1441f35d9393d075ee8a
chenhui <chenhui@followme.cn>
2021-06-03 20:10:42 +0800

    style

--- MachODemo/MachODemo/main.m
+++ MachODemo/MachODemo/main.m
@@ -5,19 +5,19 @@
 //  Created by vchan on 2021/5/31.
 //
 
-//#import <UIKit/UIKit.h>
-//#import "AppDelegate.h"
-//
-//int main(int argc, char * argv[]) {
-//    NSString * appDelegateClassName;
-//    @autoreleasepool {
-//        // Setup code that might create autoreleased objects goes here.
-//        appDelegateClassName = NSStringFromClass([AppDelegate class]);
-//    }
-//    return UIApplicationMain(argc, argv, nil, appDelegateClassName);
-//}
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
 
-#import <stdio.h>
+int main(int argc, char * argv[]) {
+    NSString * appDelegateClassName;
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+        appDelegateClassName = NSStringFromClass([AppDelegate class]);
+    }
+    return UIApplicationMain(argc, argv, nil, appDelegateClassName);
+}
+
+//#import <stdio.h>
 //int main(int argc, char * argv[]) {
 //    static int iii = 0;
 //    static int jjj = 1;
@@ -26,13 +26,13 @@
 //    return 0;
 //}
 
-extern int global_var;
-
-void func(int a);
-
-int main() {
-    int a = 100;
-    func(a + global_var);
-    printf("%d + %d",a, a);
-    return 0;
-}
+//extern int global_var;
+//
+//void func(int a);
+//
+//int main() {
+//    int a = 100;
+//    func(a + global_var);
+//    printf("%d + %d",a, a);
+//    return 0;
+//}
